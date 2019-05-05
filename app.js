eaa48c129451f2c3cbde41c83000384892607b0c
Alex <alexgmark@gmail.com>
2019-05-05 10:53:41 +0100

    Proper app.js ports/routing

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 
-app.listen(3000, function(){
+app.get('/', (req, res) => {
+	res.send('HELLO!');
+})
+
+app.listen(port, function(){
 	console.log('My app is working');
 })
